f314377b11e3fd97b749e299edcd52e026054033
manshing137 <shingshing7475@gmail.com>
2022-04-13 20:27:55 +0800

    chapter1 c

--- chapter1/app.js
+++ chapter1/app.js
@@ -1,3 +1,11 @@
+// echo "# node-tut" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/manshing137/node-tut.git
+// git push -u origin main
+
 const http = require("http");
 
 
